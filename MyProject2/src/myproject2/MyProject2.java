0bd8b245ed2b645da2a2b78e8122fce76b5cfaf7
ferdhian surya <ferdhian2416@student.uns.ac.id>
2022-08-30 16:14:53 +0700

    Create MyProject2.java

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author ferdhian surya
+ */
+public class MyProject2 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FFFFFFF     EEEEEEE     RRRRRR");
+        System.out.println("FF          EE          RR    RR");
+        System.out.println("FF          EE          RR  RRR");
+        System.out.println("FFFF        EEEEEEE     RRRR");
+        System.out.println("FF          EE          RR  R");
+        System.out.println("FF          EE          RR   RR");
+        System.out.println("FF          EEEEEEE     RR    RR");
+        // TODO code application logic here
+    }
+    
+}
+
