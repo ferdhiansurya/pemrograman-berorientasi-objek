da29e81a9ba59636bc74acde14761910ec3d3974
ferdhian surya <ferdhian2416@student.uns.ac.id>
2022-08-30 16:14:12 +0700

    Create MyProject1.java

---
+++ MyProject1/src/myproject1/MyProject1.java
@@ -0,0 +1,7 @@
+public class MyProject1 {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("Hello Java");
+        System.out.print("Java is fun");
+    }
+}
