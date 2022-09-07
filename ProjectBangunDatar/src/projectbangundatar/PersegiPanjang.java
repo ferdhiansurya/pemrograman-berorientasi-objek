62e59dd225b3319df508be35d4d9a2132a3248b3
ferdhian surya <ferdhian2416@student.uns.ac.id>
2022-09-07 14:57:45 +0700

    Create PersegiPanjang.java

---
+++ ProjectBangunDatar/src/projectbangundatar/PersegiPanjang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar;
+
+/**
+ *
+ * @author ferdhian surya
+ */
+public class PersegiPanjang {
+    int panjang;
+    int lebar;
+    
+    void kelilingPersegiPanjang() {
+        int keliling = 2 * panjang + 2 * lebar;
+        System.out.println("Keliling Persegi Panjang = " + keliling);
+    }
+    
+    void luasPersegiPanjang() {
+        int luas = panjang * lebar;
+        System.out.println("Luas Persegi Panjang = " + luas);
+    }
+}
