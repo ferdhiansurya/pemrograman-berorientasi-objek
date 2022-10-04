e55050a156fc8f4267e5cf4eb816cd4945bece7f
ferdhian surya <ferdhian2416@student.uns.ac.id>
2022-10-04 19:45:16 +0700

    Create ProjectBangunDatar2.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/ProjectBangunDatar2.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author ferdhian surya
+ */
+public class ProjectBangunDatar2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persegi x = new Persegi();
+        x.s = 5;
+        x.hitungLuas();
+        x.hitungKeliling();
+        x.S = 4.5;
+        x.hitungLuas2();
+        x.hitungKeliling2();
+
+        PersegiPanjang b = new PersegiPanjang();
+        b.p = 10;
+        b.l = 5;
+        b.hitungLuas();
+        b.hitungKeliling();
+        b.P = 3.6;
+        b.l = 8;
+        b.hitungLuas4();
+        b.hitungKeliling3();
+        b.p = 6;
+        b.L = 8.3;
+        b.hitungLuas3();
+        b.hitungKeliling4();
+        b.P = 5.6;
+        b.L = 8.8;
+        b.hitungLuas2();
+        b.hitungKeliling2();
+        
+        Lingkaran c = new Lingkaran();
+        c.r = 5;
+        c.hitungLuas();
+        c.hitungKeliling();
+        c.R = 7.4;
+        c.hitungLuas2();
+        c.hitungKeliling2();
+        
+        Segitiga d = new Segitiga();
+        d.a = 8;
+        d.t = 10;
+        d.hitungLuas();
+        d.
+                
+    }
+    
+}
